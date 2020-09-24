3fcd2b62004999198f5bf2105530d5be46a8674f
Dilpreet Singh <dilpreet1851990@gmail.com>
2020-09-24 18:03:07 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "demo.mp4");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/KrKUDlUND5E");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
